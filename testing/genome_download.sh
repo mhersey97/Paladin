01a518190a0226f5aab410086e3139078d399030
Sarah Hall <sjh1024@login01.rcchpc>
2020-02-12 20:07:20 -0500

    Added ART/download scripts

---
+++ testing/genome_download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -O acidovorax_avenae.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Acidovorax_avenae/representative/GCA_000176855.2_ASM17685v2/GCA_000176855.2_ASM17685v2_genomic.fna.gz
+wget -O escherichia_coli.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Escherichia_coli/reference/GCA_000005845.2_ASM584v2/GCA_000005845.2_ASM584v2_genomic.fna.gz
+wget -O pseudomonas_aeruginosa.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Pseudomonas_aeruginosa/reference/GCA_000006765.1_ASM676v1/GCA_000006765.1_ASM676v1_genomic.fna.gz
+wget -O micrococcus_luteus.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Micrococcus_luteus/representative/GCA_000023205.1_ASM2320v1/GCA_000023205.1_ASM2320v1_genomic.fna.gz
+wget -O clostridioides_difficile.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/Clostridioides_difficile/reference/GCA_000009205.2_ASM920v2/GCA_000009205.2_ASM920v2_genomic.fna.gz 
+wget -O candida_albicans.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/fungi/Candida_albicans/representative/GCA_000182965.3_ASM18296v3/GCA_000182965.3_ASM18296v3_genomic.fna.gz
+wget -O fusarium_oxysporum.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/fungi/Fusarium_oxysporum/representative/GCA_000149955.2_ASM14995v2/GCA_000149955.2_ASM14995v2_genomic.fna.gz
+wget -O caenorhabditis_elegans.fna.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/invertebrate/Caenorhabditis_elegans/reference/GCA_000002985.3_WBcel235/GCA_000002985.3_WBcel235_genomic.fna.gz
