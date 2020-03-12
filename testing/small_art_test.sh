9f04ab056e8fee34495a2816dfdce9e4ffb754ae
Sarah Hall <sjh1024@login01.rcchpc>
2020-03-11 23:50:27 -0400

    it is nearly midnight and i have lost my mind

--- testing/small_art_test.sh
+++ testing/small_art_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 module load linuxbrew/colsa/
 
-art_illumina -ef -i acidovorax_avenae.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outaavenae 
-art_illumina -ef -i escherichia_coli.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outecoli
-art_illumina -ef -i pesudomonas_aeruginosa.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outpaeruginosa 
-art_illumina -ef -i micrococcus_luteus.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outmluteus
-art_illumina -ef -i clostridioides_difficile.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outcdifficile 
-art_illumina -ef -i fusarium_oxysporum.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outfoxysporum 
-art_illumina -ef -i candida_albicans.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outcalbicans 
-art_illumina -ef -i caenorhabditis_elegans.fna.gz -p -l 249 -f 1 -m 250 -s 50 -o outcelegans 
-
+art_illumina -ef -i acidovorax_avenae.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outaavenaeshort 
+art_illumina -ef -i escherichia_coli.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outecolishort
+art_illumina -ef -i pesudomonas_aeruginosa.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outpaeruginosashort 
+art_illumina -ef -i micrococcus_luteus.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outmluteusshort
+art_illumina -ef -i clostridioides_difficile.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outcdifficileshort 
+art_illumina -ef -i fusarium_oxysporum.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outfoxysporumshort 
+art_illumina -ef -i candida_albicans.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outcalbicansshort 
+art_illumina -ef -i caenorhabditis_elegans.fna.gz -p -l 100 -f 1 -m 250 -s 50 -o outcelegansshort
+#longer files
